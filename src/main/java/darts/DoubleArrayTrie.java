ac6c732706e529b1b8111d44b0b3b4f8f237cd5f
haifuns <haifuns@163.com>
2022-01-19 20:05:25 +0800

    添加注释

--- src/main/java/darts/DoubleArrayTrie.java
+++ src/main/java/darts/DoubleArrayTrie.java
@@ -26,6 +26,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+@SuppressWarnings("all")
 public class DoubleArrayTrie {
 	private final static int BUF_SIZE = 16384;
 	private final static int UNIT_SIZE = 8; // size of int + int
@@ -35,20 +36,31 @@ public class DoubleArrayTrie {
 		int depth;
 		int left;
 		int right;
+
+		@Override
+		public String toString() {
+			return "Node{" +
+					"code=" + code +
+					", char=" + new Character((char)(code - 1)) +
+					", depth=" + depth +
+					", left=" + left +
+					", right=" + right +
+					'}';
+		}
 	};
 
-	private int check[];
-	private int base[];
+	private int check[]; // 核心数组，用来校验
+	private int base[]; //核心数组，保存trie树信息
 
-	private boolean used[];
-	private int size;
-	private int allocSize;
-	private List<String> key;
-	private int keySize;
-	private int length[];
-	private int value[];
+	private boolean used[]; // 已经使用的begin, insert用到
+	private int size; // 数组使用长度
+	private int allocSize; // 数组长度
+	private List<String> key; // 词典
+	private int keySize; // 词个数
+	private int length[]; // 没用，一直是空
+	private int value[]; // 没用，一直是空
 	private int progress;
-	private int nextCheckPos;
+	private int nextCheckPos; // 下一个可以使用的check字符
 	// boolean no_delete_;
 	int error_;
 
@@ -73,43 +85,50 @@ public class DoubleArrayTrie {
 	}
 
 	private int fetch(Node parent, List<Node> siblings) {
+		System.out.println("fetch start");
+		System.out.println("parent_node -> " + parent);
+
 		if (error_ < 0)
 			return 0;
 
 		int prev = 0;
 
+		// 根节点 [0-6)
 		for (int i = parent.left; i < parent.right; i++) {
 			if ((length != null ? length[i] : key.get(i).length()) < parent.depth)
 				continue;
 
+			// 一举, .. , 万能
 			String tmp = key.get(i);
 
-			int cur = 0;
-			if ((length != null ? length[i] : tmp.length()) != parent.depth)
-				cur = (int) tmp.charAt(parent.depth) + 1;
+			int cur = 0; // 当前字符code + 1
+			if ((length != null ? length[i] : tmp.length()) != parent.depth) // depth 0
+				cur = (int) tmp.charAt(parent.depth) + 1; // char[0] + 1
 
 			if (prev > cur) {
 				error_ = -3;
 				return 0;
 			}
 
-			if (cur != prev || siblings.size() == 0) {
+			if (cur != prev || siblings.size() == 0) { // 一、万
 				Node tmp_node = new Node();
-				tmp_node.depth = parent.depth + 1;
+				tmp_node.depth = parent.depth + 1; // depth + 1
 				tmp_node.code = cur;
-				tmp_node.left = i;
+				tmp_node.left = i; // 第一次出现时在词典中的位置
 				if (siblings.size() != 0)
-					siblings.get(siblings.size() - 1).right = i;
+					siblings.get(siblings.size() - 1).right = i; // 兄弟节点中前一个字符right=当前left
 
 				siblings.add(tmp_node);
+				System.out.println("child_node -> " + tmp_node);
 			}
 
 			prev = cur;
 		}
 
 		if (siblings.size() != 0)
-			siblings.get(siblings.size() - 1).right = parent.right;
+			siblings.get(siblings.size() - 1).right = parent.right; // 兄弟节点最后一个字符right=父节点right
 
+		System.out.println("fetch end\n------");
 		return siblings.size();
 	}
 
@@ -119,7 +138,7 @@ public class DoubleArrayTrie {
 
 		int begin = 0;
 		int pos = ((siblings.get(0).code + 1 > nextCheckPos) ? siblings.get(0).code + 1
-				: nextCheckPos) - 1;
+				: nextCheckPos) - 1; // 19969
 		int nonzero_num = 0;
 		int first = 0;
 
@@ -140,7 +159,7 @@ public class DoubleArrayTrie {
 				first = 1;
 			}
 
-			begin = pos - siblings.get(0).code;
+			begin = pos - siblings.get(0).code; // 距离第一个兄弟节点的距离
 			if (allocSize <= (begin + siblings.get(siblings.size() - 1).code)) {
 				// progress can be zero
 				double l = (1.05 > 1.0 * keySize / (progress + 1)) ? 1.05 : 1.0
@@ -152,7 +171,7 @@ public class DoubleArrayTrie {
 				continue;
 
 			for (int i = 1; i < siblings.size(); i++)
-				if (check[begin + siblings.get(i).code] != 0)
+				if (check[begin + siblings.get(i).code] != 0) // 找到一个begin，check[每一个兄弟节点+begin]=0, 即找到了兄弟节点个空位置
 					continue outer;
 
 			break;
@@ -172,14 +191,14 @@ public class DoubleArrayTrie {
 				: begin + siblings.get(siblings.size() - 1).code + 1;
 
 		for (int i = 0; i < siblings.size(); i++)
-			check[begin + siblings.get(i).code] = begin;
+			check[begin + siblings.get(i).code] = begin; // 找到的位置check值设置为begin
 
 		for (int i = 0; i < siblings.size(); i++) {
 			List<Node> new_siblings = new ArrayList<Node>();
 
 			if (fetch(siblings.get(i), new_siblings) == 0) {
 				base[begin + siblings.get(i).code] = (value != null) ? (-value[siblings
-						.get(i).left] - 1) : (-siblings.get(i).left - 1);
+						.get(i).left] - 1) : (-siblings.get(i).left - 1); // 如果没有子节点, 说明词语终结了，base值设置为负
 
 				if (value != null && (-value[siblings.get(i).left] - 1) >= 0) {
 					error_ = -2;
@@ -190,8 +209,8 @@ public class DoubleArrayTrie {
 				// if (progress_func_) (*progress_func_) (progress,
 				// keySize);
 			} else {
-				int h = insert(new_siblings);
-				base[begin + siblings.get(i).code] = h;
+				int h = insert(new_siblings); // 如果有子节点递归插入
+				base[begin + siblings.get(i).code] = h; // base值设置为子节点begin值, 所有子节点begin值相同
 			}
 		}
 		return begin;
@@ -264,7 +283,7 @@ public class DoubleArrayTrie {
 
 		resize(65536 * 32);
 
-		base[0] = 1;
+		base[0] = 1; // root节点 base[0]=1 check[0]=0
 		nextCheckPos = 0;
 
 		Node root_node = new Node();
@@ -369,7 +388,7 @@ public class DoubleArrayTrie {
 
 		char[] keyChars = key.toCharArray();
 
-		int b = base[nodePos];
+		int b = base[nodePos]; // 初始位置, base[0]=1
 		int n;
 		int p;
 
@@ -381,7 +400,7 @@ public class DoubleArrayTrie {
 				result.add(-n - 1);
 			}
 
-			p = b + (int) (keyChars[i]) + 1;
+			p = b + (int) (keyChars[i]) + 1; // base[0]+char(一)+1=19970
 			if (b == check[p])
 				b = base[p];
 			else
--- src/test/java/DoubleArrayTrieTest.java
+++ src/test/java/DoubleArrayTrieTest.java
@@ -10,52 +10,42 @@ import java.util.Set;
 
 public class DoubleArrayTrieTest {
 
-    public static void main(String[] args) throws IOException
-    {
+    public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new FileReader("./small.dic"));
         String line;
-        List<String> words = new ArrayList<String>();
-        Set<Character> charset = new HashSet<Character>();
-        while ((line = reader.readLine()) != null)
-        {
+        List<String> words = new ArrayList<>();
+        Set<Character> charset = new HashSet<>();
+        while ((line = reader.readLine()) != null) {
             words.add(line);
             // 制作一份码表debug
-            for (char c : line.toCharArray())
-            {
+            for (char c : line.toCharArray()) {
                 charset.add(c);
             }
         }
         reader.close();
+
         // 这个字典如果要加入新词必须按字典序，参考下面的代码
         /*Collections.sort(words);
         BufferedWriter writer = new BufferedWriter(new FileWriter("./data/sorted.dic", false));
-        for (String w : words)
-        {
+        for (String w : words) {
             writer.write(w);
             writer.newLine();
         }*/
+
         System.out.println("字典词条：" + words.size());
 
-        {
-            String infoCharsetValue = "";
-            String infoCharsetCode = "";
-            for (Character c : charset)
-            {
-                infoCharsetValue += c.charValue() + "    ";
-                infoCharsetCode += (int)c.charValue() + " ";
-            }
-            infoCharsetValue += '\n';
-            infoCharsetCode += '\n';
-            System.out.print(infoCharsetValue);
-            System.out.print(infoCharsetCode);
+        StringBuilder infoCharsetValue = new StringBuilder();
+        for (Character c : charset) {
+            infoCharsetValue.append(c.charValue()).append("[").append((int) c).append("] ");
         }
+
+        System.out.println(infoCharsetValue + "\n");
 
         DoubleArrayTrie dat = new DoubleArrayTrie();
         System.out.println("是否错误: " + dat.build(words));
-        System.out.println(dat);
+
         List<Integer> integerList = dat.commonPrefixSearch("一举成名天下知");
-        for (int index : integerList)
-        {
+        for (int index : integerList) {
             System.out.println(words.get(index));
         }
     }
